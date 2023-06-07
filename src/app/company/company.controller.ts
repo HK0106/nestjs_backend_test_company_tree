0f1bf3243b3b3a93336667f23a0b67f7040bdf56
MaiDX <cangao1504@gmail.com>
2023-06-08 02:53:11 +0700

    Init solution

--- src/app/company/company.controller.ts
+++ src/app/company/company.controller.ts
@@ -1,7 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { CompanyService } from './company.service';
 
 @Controller('companies')
-export class CompanyController {
-  constructor(private readonly companyService: CompanyService) {}
-}
+export class CompanyController {}
